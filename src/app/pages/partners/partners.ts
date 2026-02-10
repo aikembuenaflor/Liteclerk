b7d56cda443ad8547652809c3be13d5df09e84fd
aikembuenaflor <aikembuenaflor09@gmail.com>
2026-02-10 12:53:00 +0000

    designed the Partners Page

--- src/app/pages/partners/partners.ts
+++ src/app/pages/partners/partners.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Cta } from "../../components/cta/cta";
 
 @Component({
   selector: 'app-partners',
-  imports: [],
+  imports: [Cta],
   templateUrl: './partners.html',
   styleUrl: './partners.css',
 })
